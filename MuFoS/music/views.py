2db7f2b8f3d36a1e079bd280416262818c2d6416
Artiksn <Artimen916@gmail.com>
2023-06-20 14:40:04 +0300

    new

---
+++ MuFoS/music/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+
+def base(request):
+    return render(request,'music/main.html',{'title': "Главная страница"})
+
+def category_phonk(request,category_id):
+    songs = Music.objects.filter(id = category_id)
+    path_phonk = Music.objects.get(id=category_id)
+    return render(request,'music/Phonk.html',{'title': "Музыка", 'songs': songs,'path_phonk': path_phonk})
